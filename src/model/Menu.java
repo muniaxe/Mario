434dc24bde977d2d125242ce3c7afe75c278f9a7
RobbertBobbert <61043357+RobbertBobbert@users.noreply.github.com>
2020-03-31 11:06:23 +0200

    Authors

--- src/model/Menu.java
+++ src/model/Menu.java
@@ -2,11 +2,19 @@ package model;
 
 import java.util.ArrayList;
 
+/*
+@Authors
+Robert Pallesen
+Mathias Hvid
+Emil Dyrhøi Tolderlund Jørgensen
+Jack Hagedorn Jensen
+ */
+
 public class Menu {
 
     private ArrayList<Pizza> pizzaArray = new ArrayList<>();
 
-    public void addPizza(Pizza pizza){
+    public void addPizza(Pizza pizza) {
         pizzaArray.add(pizza);
     }
 
@@ -18,5 +26,14 @@ public class Menu {
             returnString += "\n";
         }
         return returnString;
+    }
+
+    public Pizza getPizzaById(int id) {
+        for (Pizza pizza : pizzaArray) {
+            if (pizza.getId() == id) {
+                return pizza;
+            }
+        }
+        return null;
     }
 }
