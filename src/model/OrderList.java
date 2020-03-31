2230631bdd0dc80c9aa72e04fb634bbc27f099a9
RobbertBobbert <61043357+RobbertBobbert@users.noreply.github.com>
2020-03-31 11:05:33 +0200

    getOrderById

--- src/model/OrderList.java
+++ src/model/OrderList.java
@@ -3,18 +3,26 @@ package model;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/*
+@Authors
+Robert Pallesen
+Mathias Hvid
+Emil Dyrhøi Tolderlund Jørgensen
+Jack Hagedorn Jensen
+ */
+
 public class OrderList {
     private ArrayList<Order> orders = new ArrayList<>();
 
-    public void addOrder(Order order){
+    public void addOrder(Order order) {
         orders.add(order);
     }
 
-    public void showUnfinishedOrders(){
+    public void showUnfinishedOrders() {
         Collections.sort(orders);
         OrderList unfinishedOrders = new OrderList();
-        for(Order order : orders) {
-            if(order.isDone()) continue;
+        for (Order order : orders) {
+            if (order.isDone()) continue;
             unfinishedOrders.addOrder(order);
         }
 
@@ -30,6 +38,15 @@ public class OrderList {
             returnString += "\n";
         }
         return returnString;
+    }
+
+    public Order getOrderById(int id) {
+        for (Order order : orders) {
+            if (order.getId() == id) {
+                return order;
+            }
+        }
+        return null;
     }
 
     public static void saveOrders() {
